951d42f74de82b0e3f8e8921b7d6e36c14d1b21a
helena-varela <helena.varelakg@gmail.com>
2026-06-25 18:25:56 -0300

    adicionando regras de negocio, incializacao de atributos e getters da classe treino

---
+++ src/Treino.cpp
@@ -0,0 +1,36 @@
+#include "Treino.hpp"
+#include <iostream>
+
+// construtor
+Treino::Treino(std::string focoMuscular, std::list<std::string> exercicios, int duracaoEstimadaMin){
+    // 1º regras de negócio: a lista de exercícios não pode estar vazia
+    if (exercicios.empty())
+    {
+        throw std::invalid_argument("O treino deve ter pelo menos um exercício");
+    }
+    
+    // 2º regra de negócio: a duração deve ser maior que 0 minutos
+    if (duracaoEstimadaMin <= 0)
+    {
+        throw std::invalid_argument("O treino deve ter duração maior que 0 minutos");
+    }
+    
+    // inicialização
+    this -> focoMuscular = focoMuscular;
+    this -> exercicios = exercicios;
+    this -> duracaoEstimadaMin = duracaoEstimadaMin;
+}
+
+
+// getters
+std::string Treino::getFoco(){
+    return this -> focoMuscular;
+};
+
+std::list<std::string> Treino::getExercicios(){
+    return this -> exercicios;
+}
+
+int Treino::getDuracao(){
+    return this -> duracaoEstimadaMin;
+}
