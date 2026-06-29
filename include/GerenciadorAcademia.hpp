5e0fbc2249ed923f2811be5d92313a0cd130e30c
helena-varela <helena.varelakg@gmail.com>
2026-06-29 12:10:52 -0300

    iniciando salvamento de arquivos

--- include/GerenciadorAcademia.hpp
+++ include/GerenciadorAcademia.hpp
@@ -9,21 +9,32 @@
 #include "PlanoMensal.hpp"
 #include "PlanoAnual.hpp"
 #include "AcademiaException.hpp"
+#include "Treino.hpp"
 
 class GerenciadorAcademia
 {
     private:
         std::vector<Cliente*> clientes;
         std::vector<Instrutor*> instrutores;
+        std::vector<Plano*> planosCadastrados;
+        std::vector<Treino*> treinosCadastrados;
+
+        Instrutor* descobrirInstrutorDoCliente(Cliente* c);
     public:
         void cadastrarCliente(Cliente* c);
         Cliente* consultarCliente(const std::string& cpf);
         Cliente* consultarClienteBase(const int& codigoMatricula);
         void atualizarCliente(const std::string& cpf, Cliente* novoC);
         void removerCliente(const std::string& cpf);
+
+        // salvar e carregar arquivos
         void salvarEmArquivo(std::string nomeArquivo);
         void carregarDeArquivo(std::string nomeArquivo);
 
+        // funções de busca auxiliares
+        Plano* buscarPlano(const std::string& id);
+        Treino* buscarTreino(const std::string& id);
+        Instrutor* buscarInstrutor(const std::string& cpf);
 };
 
 #endif
